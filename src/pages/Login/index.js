035d16aa810830bcee5a1130659ee1f994064a97
Luiz Claudio de Almeida Vianna <lcavianna@hotmail.com>
2024-07-23 13:49:09 -0300

    efetuando o login na api, recebendo o token e armazenando no localstorage

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,16 +1,50 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import './login.css'
+import api from '../../services/api';
+import{ useNavigate } from 'react-router-dom';
+
+
 import logoImagem from '../../assets/logo-imagem.png';
 
 export default function Login(){
+
+    const [UserName, setUserName] = useState('');
+    const [Password, setPassword] = useState('');
+
+    const navigate = useNavigate()
+
+    async function login(event){
+        event.preventDefault();
+        const data = {
+            UserName, Password
+        };
+        
+         try{
+                console.log(data);
+                 const response = await api.post('/login', data);
+                 localStorage.setItem('UserName',data.UserName);
+                 localStorage.setItem('token', response.data);
+                 navigate('/students')
+        }
+        catch(error){
+            alert('O login falhou ' + error)
+        }
+    }
+
     return(
            <div className='login-container'>
                 <section className='form'>
                     <img src={logoImagem} alt="login" id="img1" />
-                    <form>
+                    <form onSubmit={login}>
                         <h1>Login User</h1>
-                        <input placeholder='Login' />
-                        <input placeholder='Password' />
+                        <input placeholder='Login'
+                        value={UserName}
+                        onChange={u=>setUserName(u.target.value)} />
+
+                        <input placeholder='Password'
+                        value={Password}
+                        onChange={u=>setPassword(u.target.value)} />
+
                         <button className='button' type='submit'>Login</button>
                     </form>
                 </section>
--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 //BASE DA URL: https://localhost:7216/
 
 const api = axios.create({
-    baseURL:'https://localhost:7216/'
+    baseURL:'https://localhost:7216/',
 });
 
 
